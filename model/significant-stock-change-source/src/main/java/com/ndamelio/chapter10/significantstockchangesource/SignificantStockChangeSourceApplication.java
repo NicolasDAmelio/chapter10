929345f7f948479e212eb4175ee5c0d95d9298bd
Nicolas D'Amelio <ndamelio@capitaria.com>
2018-08-20 12:08:17 -0300

    asd

---
+++ model/significant-stock-change-source/src/main/java/com/ndamelio/chapter10/significantstockchangesource/SignificantStockChangeSourceApplication.java
@@ -0,0 +1,12 @@
+package com.ndamelio.chapter10.significantstockchangesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignificantStockChangeSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignificantStockChangeSourceApplication.class, args);
+    }
+}
